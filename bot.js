04b6ed5148f6b4efeb4417b62e5228d8aa458f12
GamzXT <44983954+GamzXT@users.noreply.github.com>
2019-06-16 19:05:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
 const djs = require('discord.js');
 const client = new djs.Client();
 
-client.on('guildMemberAdd', member => {
-	const mem = member.guild.members.find('id', member.user.id);
+client.on('guildMemberAdd', function(mem){
 	mem.addRole('589823111893876756');
-})
+});
 
 client.login(process.env.BOT_TOKEN);
